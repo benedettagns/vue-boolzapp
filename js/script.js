f82d995f7e2b52d81c77fdf3a9abd982c62a8a2e
benedettagns <benedetta.genise01@gmail.com>
2022-01-24 21:55:06 +0100

    correzioni

--- js/script.js
+++ js/script.js
@@ -92,6 +92,23 @@ new Vue({
         nuovoMex: '',
     },
     
+    /*Visualizzazione dinamica dei messaggi: 
+    tramite la direttiva v-for, visualizzare tutti i messaggi 
+    relativi al contatto attivo all’interno del pannello della 
+    conversazione
+    Click sul contatto mostra la conversazione del contatto 
+    cliccato
+    */
+
+    /*Risposta dall’interlocutore: ad ogni inserimento di un 
+    messaggio, l’utente riceverà un “ok” come risposta, che 
+    apparirà dopo 1 secondo.*/
+
+    /*Ricerca utenti: scrivendo qualcosa nell’input a 
+    sinistra, vengono visualizzati solo i contatti il cui nome 
+    contiene le lettere inserite (es, Marco, Matteo Martina -> 
+    Scrivo “mar” rimangono solo Marco e Martina)*/
+
     methods: {
         immaginiUtenti(utenti){
             return `img/avatar${utenti.avatar}.jpg`
